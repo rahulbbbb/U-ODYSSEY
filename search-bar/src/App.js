c3b7c2b34f38b0fea54d0a1eb3d086c18a6e2b3f
Rahulbbbb <beingbankoti@gmail.com>
2023-05-23 02:03:55 +0530

    first commit

---
+++ search-bar/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SearchBar from './searchBar';
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Google Search</h1>
+      <SearchBar />
+    </div>
+  );
+};
+
+export default App;
